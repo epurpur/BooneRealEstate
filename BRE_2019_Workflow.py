5af2a77432ac05fc60b81fcb977cd051d692ea8c
Erich Purpur <epurpur@gmail.com>
2019-12-10 12:00:06 -0500

    Add files via upload

--- BRE_2019_Workflow.py
+++ BRE_2019_Workflow.py
@@ -8,7 +8,7 @@ Created on Thu Dec  5 16:14:07 2019
 
 
 import geopandas as gpd
-
+import pandas as pd
 
 ##### STARTING FROM THE TOP, READ THE 'AllResidential2019' FILE INTO DATAFRAME
 ##
@@ -48,19 +48,9 @@ import geopandas as gpd
 filepath = '/Users/ep9k/Desktop/BRE/BRE 2019/allkeepers_2019.gpkg'
 
 all_keepers_df = gpd.read_file(filepath, layer='allkeepers_2019')
-
 all_keepers_df.drop_duplicates(subset='nparno', keep='first', inplace=True)
 
 
-####DROP UNNEEDED COLUMNS FROM DATAFRAME
-
-columns_to_drop = ['id_0', 'id', 'gnisid', 'maddpref', 'maddrno',
-       'maddstname', 'maddstr', 'maddstsuf', 'maddsttyp', 'mapref', 
-       'munit', 'ownfrst', 'ownlast', 'owntype', 'parusedsc2', 'revdatetx',
-       'saddpref', 'scity', 'structno', 'subowntype', 'subsurfown', 'sunit',
-       'szip']
-
-all_keepers_df.drop(columns_to_drop, inplace=True, axis=1)
 
 
 #### READ HELPER GPKG FILE TO JOIN MATT'S COLUMNS
@@ -68,10 +58,66 @@ all_keepers_df.drop(columns_to_drop, inplace=True, axis=1)
 filepath = '/Users/ep9k/Desktop/BRE/BRE 2019/keepers_2019.gpkg'
 helper_df = gpd.read_file(filepath, layer='keepers_2019')
 
+####MERGE these columns from helper_df to all_keepers_df: '2+ Removed', 'Excluded_Subdivisions', 'Owner_moved', 'sold_in_last_year', 'No_Change', 'VacantLand', 'VacantLandValue'
 
-#### START HERE. JOIN these columns to all_keepers_df: '2+ Removed', 'Excluded_Subdivisions', 'Owner_moved', 'sold_in_last_year', 'No_Change', 'VacantLand', 'VacantLandValue'
+all_keepers_df = all_keepers_df.merge(helper_df, left_on='nparno', right_on='NPARNO')
 
-print(helper_df.columns)
+####DROP UNNEEDED COLUMNS FROM DATAFRAME
+
+all_keepers_df['geometry'] = all_keepers_df['geometry_x']   #rename this column since I have duplicate geometry columns
+
+columns_to_drop = ['id_0', 'id', 'gnisid', 'maddpref', 'maddrno',
+       'maddstname', 'maddstr', 'maddstsuf', 'maddsttyp', 'mapref', 
+       'munit', 'ownfrst', 'ownlast', 'owntype', 'parusedsc2', 'revdatetx',
+       'saddpref', 'scity', 'structno', 'subowntype', 'subsurfown', 'sunit',
+       'szip', 'ALTPARNO', 'CNTYFIPS', 'CNTYNAME', 'GISACRES', 
+       'IMPROVVAL', 'LANDVAL', 'LEGDECFULL', 'MAILADD', 'MCITY', 'MSTATE', 
+       'MULTISTRUC', 'MZIP', 'NPARNO', 'OWNNAME', 'OWNNAME2', 'OWNTYPE', 'PARNO', 
+       'PARUSECD2', 'PARUSECODE', 'PARUSEDESC', 'PARVAL', 'PARVALTYPE', 
+       'SADDSTNAME', 'SADDSTR', 'SADDSTSUF', 'SADDSTTYP', 'SALEDATE', 'SALEDATETX', 
+       'SITEADD', 'SOURCEAGNT', 'SOURCEDATE', 'SOURCEDATX', 'SSTATE', 'STCNTYFIPS', 
+       'STFIPS', 'STNAME', 'STRUCT', 'STRUCTYEAR', 'SUBDIVISIO', 'TRANSFDATE', 
+       'OWNER_NAME', '2019AllKee', 'layer', 'path', 'geometry_x', 'geometry_y']
+
+all_keepers_df.drop(columns_to_drop, inplace=True, axis=1)
+
+
+
+#### EXTRACT MATCHING CONDOS FROM 2019 KEEPERS####
+condos_dataset_path = '/Users/ep9k/Desktop/BRE/MattCondoAddressList.xlsx'
+
+condos_df = pd.read_excel(condos_dataset_path)
+
+#check by Parcel ID (Parcel ID (PIN)/ALTPARNO)
+condo_parcel_id_list = condos_df['Parcel ID (PIN)'].tolist()
+
+#all_keepers_df.drop(all_keepers_df[all_keepers_df['altparno'].isin(condo_parcel_id_list)].index, inplace=True)  #0 matches?
+print(all_keepers_df['2901821323000'.isin(condo_parcel_id_list)])
+    
+#2901821323000
+
+
+
+
+
+
+
+
+####CONVERT DATAFRAME BACK TO GEODATAFRAME
+#
+#all_keepers_df = gpd.GeoDataFrame(all_keepers_df,
+#                                  crs={'init': 'epsg: 2264'},
+#                                  geometry = all_keepers_df['geometry'])
+#
+#
+#all_keepers_df.to_file('/Users/ep9k/Desktop/test_out2.gpkg', driver='GPKG')
+#
+#
+#
+#
+#
+#
+####LAST STEP IS TO MANUALLY REMOVE UNDESIRABLE PARCELS SUCH AS CHURCHES, COMMERCIAL, BY HAND IN QGIS
 
 
 
