8f9ef23923386bae0e3495faf243c3ee045cc360
Erich Purpur <epurpur@gmail.com>
2024-05-20 17:01:25 -0400

    Add files via upload

---
+++ BRE_Data_Map.py
@@ -0,0 +1,107 @@
+
+
+
+######FIELDS NEEDED
+"""
+Xnearest feature
+Xcreek
+Xsubdivision
+Xcustom area
+Creek checkbox?
+XCounty Name
+XZone
+Brand?
+Agent?
+"""
+
+
+
+
+
+## REASSIGN CUSTOM_AREA value to salesforce ID of corresponding custom area
+bannerelk_points['CUSTOM_AREA__C'] = 'a2E3u000000fPH4EAM'
+bannerelk_shapes['CUSTOM_AREA__C'] = 'a2E3u000000fPH4EAM'
+beechmountain_points['CUSTOM_AREA__C'] = 'a2E3u000000fPH5EAM'
+beechmountain_shapes['CUSTOM_AREA__C'] = 'a2E3u000000fPH5EAM'
+bethel_points['CUSTOM_AREA__C'] = 'a2E3u000000fPH6EAM'
+bethel_shapes['CUSTOM_AREA__C'] = 'a2E3u000000fPH6EAM'
+blowingrock_points['CUSTOM_AREA__C'] = 'a2E3u000000fPH7EAM'
+blowingrock_shapes['CUSTOM_AREA__C'] = 'a2E3u000000fPH7EAM'
+boone_points['CUSTOM_AREA__C'] = 'a2E3u000000fPH8EAM'
+boone_shapes['CUSTOM_AREA__C'] = 'a2E3u000000fPH8EAM'
+butler_points['CUSTOM_AREA__C'] = 'a2E3u000000fPTxEAM'
+butler_shapes['CUSTOM_AREA__C'] = 'a2E3u000000fPTxEAM'
+collettsville_points['CUSTOM_AREA__C'] = 'a2E3u000000fPH9EAM'
+collettsville_shapes['CUSTOM_AREA__C'] = 'a2E3u000000fPH9EAM'
+creston_points['CUSTOM_AREA__C'] = 'a2E3u000000fPHAEA2'
+creston_shapes['CUSTOM_AREA__C'] = 'a2E3u000000fPHAEA2'
+crossnore_points['CUSTOM_AREA__C'] = 'a2E3u000000fPHBEA2'
+crossnore_shapes['CUSTOM_AREA__C'] = 'a2E3u000000fPHBEA2'
+crumpler_points['CUSTOM_AREA__C'] = 'a2E3u000000fPHCEA2'
+crumpler_shapes['CUSTOM_AREA__C'] = 'a2E3u000000fPHCEA2'
+deepgap_points['CUSTOM_AREA__C'] = 'a2E3u000000fPHDEA2'
+deepgap_shapes['CUSTOM_AREA__C'] = 'a2E3u000000fPHDEA2'
+elkpark_points['CUSTOM_AREA__C'] = 'a2E3u000000fPHEEA2'
+elkpark_shapes['CUSTOM_AREA__C'] = 'a2E3u000000fPHEEA2'
+fleetwood_points['CUSTOM_AREA__C'] = 'a2E3u000000fPHFEA2'
+fleetwood_shapes['CUSTOM_AREA__C'] = 'a2E3u000000fPHFEA2'
+foscoe_points['CUSTOM_AREA__C'] = 'a2E3u000000fPHGEA2'
+foscoe_shapes['CUSTOM_AREA__C'] = 'a2E3u000000fPHGEA2'
+glendalesprings_points['CUSTOM_AREA__C'] = 'a2E3u000000fPHHEA2'
+grassycreek_points['CUSTOM_AREA__C'] = 'a2E3u000000fPHIEA2'
+grassycreek_shapes['CUSTOM_AREA__C'] = 'a2E3u000000fPHIEA2'
+hampton_points['CUSTOM_AREA__C'] = 'a2E3u000000fPTyEAM'
+hampton_shapes['CUSTOM_AREA__C'] = 'a2E3u000000fPTyEAM'
+jefferson_points['CUSTOM_AREA__C'] = 'a2E3u000000fPHJEA2'
+jefferson_shapes['CUSTOM_AREA__C'] = 'a2E3u000000fPHJEA2'
+lansing_points['CUSTOM_AREA__C'] = 'a2E3u000000fPHKEA2'
+lansing_shapes['CUSTOM_AREA__C'] = 'a2E3u000000fPHKEA2'
+laurelbloomery_points['CUSTOM_AREA__C'] = 'a2E3u000000fPTzEAM'
+laurelbloomery_shapes['CUSTOM_AREA__C'] = 'a2E3u000000fPTzEAM'
+laurelsprings_points['CUSTOM_AREA__C'] = 'a2E3u000000fPHLEA2'
+laurelsprings_shapes['CUSTOM_AREA__C'] = 'a2E3u000000fPHLEA2'
+linville_points['CUSTOM_AREA__C'] = 'a2E3u000000fPHMEA2'
+linville_shapes['CUSTOM_AREA__C'] = 'a2E3u000000fPHMEA2'
+matney_points['CUSTOM_AREA__C'] = 'a2E3u000000fPHNEA2'
+matney_shapes['CUSTOM_AREA__C'] = 'a2E3u000000fPHNEA2'
+minneapolis_points['CUSTOM_AREA__C'] = 'a2E3u000000fPHOEA2'
+minneapolis_shapes['CUSTOM_AREA__C'] = 'a2E3u000000fPHOEA2'
+mountaincity_points['CUSTOM_AREA__C'] = 'a2E3u000000fPU0EAM'
+mountaincity_shapes['CUSTOM_AREA__C'] = 'a2E3u000000fPU0EAM'
+newland_points['CUSTOM_AREA__C'] = 'a2E3u000000fPHdEAM'
+newland_shapes['CUSTOM_AREA__C'] = 'a2E3u000000fPHdEAM'
+pineola_points['CUSTOM_AREA__C'] = 'a2E3u000000fPHPEA2'
+pineola_shapes['CUSTOM_AREA__C'] = 'a2E3u000000fPHPEA2'
+pineycreek_points['CUSTOM_AREA__C'] = 'a2E3u000000fPHQEA2'
+pineycreek_shapes['CUSTOM_AREA__C'] = 'a2E3u000000fPHQEA2'
+roanmountain_points['CUSTOM_AREA__C'] = 'a2E3u000000fPU1EAM'
+roanmountain_shapes['CUSTOM_AREA__C'] = 'a2E3u000000fPU1EAM'
+scottville_shapes['CUSTOM_AREA__C'] = 'a2E3u000000fPHREA2'
+sevendevils_points['CUSTOM_AREA__C'] = 'a2E3u000000fPHSEA2'
+sevendevils_shapes['CUSTOM_AREA__C'] = 'a2E3u000000fPHSEA2'
+shadyvalley_points['CUSTOM_AREA__C'] = 'a2E3u000000fPU2EAM'
+shadyvalley_shapes['CUSTOM_AREA__C'] = 'a2E3u000000fPU2EAM'
+sparta_points['CUSTOM_AREA__C'] = 'a2E3u000000fPHTEA2'
+sparta_shapes['CUSTOM_AREA__C'] = 'a2E3u000000fPHTEA2'
+sugargrove_points['CUSTOM_AREA__C'] = 'a2E3u000000fPHUEA2'
+sugargrove_shapes['CUSTOM_AREA__C'] = 'a2E3u000000fPHUEA2'
+sugarmountain_points['CUSTOM_AREA__C'] = 'a2E3u000000fPHVEA2'
+sugarmountain_shapes['CUSTOM_AREA__C'] = 'a2E3u000000fPHVEA2'
+todd_points['CUSTOM_AREA__C'] = 'a2E3u000000fPHWEA2'
+todd_shapes['CUSTOM_AREA__C'] = 'a2E3u000000fPHWEA2'
+trade_points['CUSTOM_AREA__C'] = 'a2E3u000000fPU9EAM'
+trade_shapes['CUSTOM_AREA__C'] = 'a2E3u000000fPU9EAM'
+triplett_points['CUSTOM_AREA__C'] = 'a2E3u000000fPHXEA2'
+triplett_shapes['CUSTOM_AREA__C'] = 'a2E3u000000fPHXEA2'
+vallecrucis_points['CUSTOM_AREA__C'] = 'a2E3u000000fPHYEA2'
+vallecrucis_shapes['CUSTOM_AREA__C'] = 'a2E3u000000fPHYEA2'
+vilas_points['CUSTOM_AREA__C'] = 'a2E3u000000fPHZEA2'
+vilas_shapes['CUSTOM_AREA__C'] = 'a2E3u000000fPHZEA2'
+warrensville_points['CUSTOM_AREA__C'] = 'a2E3u000000fPHaEAM'
+warrensville_shapes['CUSTOM_AREA__C'] = 'a2E3u000000fPHaEAM'
+wataugalake_points['CUSTOM_AREA__C'] = 'a2E3u000000fPU3EAM'
+wataugalake_shapes['CUSTOM_AREA__C'] = 'a2E3u000000fPU3EAM'
+westjefferson_points['CUSTOM_AREA__C'] = 'a2E3u000000fPHbEAM'
+westjefferson_shapes['CUSTOM_AREA__C'] = 'a2E3u000000fPHbEAM'
+zionville_points['CUSTOM_AREA__C'] = 'a2E3u000000fPHcEAM'
+zionville_shapes['CUSTOM_AREA__C'] = 'a2E3u000000fPHcEAM'
